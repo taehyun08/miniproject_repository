04d7b6c9bb505276a04651ab36eaf7880325f01e
dkdlxl <dkdlxl@DESKTOP-0479I92>
2023-08-29 10:10:06 +0900

    refactor: control 단을 spring framework을 사용하여 리팩토링(미완성)

---
+++ 06.Model2MVCShop/src/main/java/com/model2/mvc/web/product/ProductController.java
@@ -0,0 +1,164 @@
+package com.model2.mvc.web.product;
+
+import java.util.Map;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.model2.mvc.common.Page;
+import com.model2.mvc.common.Search;
+import com.model2.mvc.service.domain.Product;
+import com.model2.mvc.service.domain.User;
+import com.model2.mvc.service.product.ProductService;
+import com.model2.mvc.service.user.UserService;
+
+
+//==> 회원관리 Controller
+@Controller
+public class ProductController {
+	
+	///Field
+	@Autowired
+	@Qualifier("productServiceImpl")
+	private ProductService productService;
+	//setter Method 구현 않음
+		
+	public ProductController(){
+		System.out.println(this.getClass());
+	}
+	
+	//==> classpath:config/common.properties  ,  classpath:config/commonservice.xml 참조 할것
+	//==> 아래의 두개를 주석을 풀어 의미를 확인 할것
+	@Value("#{commonProperties['pageUnit']}")
+	//@Value("#{commonProperties['pageUnit'] ?: 3}")
+	int pageUnit;
+	
+	@Value("#{commonProperties['pageSize']}")
+	//@Value("#{commonProperties['pageSize'] ?: 2}")
+	int pageSize;
+	
+	
+	@RequestMapping("/addProductView.do")
+	public String addProductView() throws Exception {
+
+		System.out.println("/addProductView.do");
+		
+		return "redirect:/product/addProductView.jsp";
+	}
+	
+	@RequestMapping("/addProduct.do")
+	public String addProduct( @ModelAttribute("product") Product product ) throws Exception {
+
+		System.out.println("/addProduct.do");
+		//Business Logic
+		productService.addProduct(product);
+		
+		return "redirect:/product/addProduct.jsp";
+	}
+	
+	@RequestMapping("/getProduct.do")
+	public String getProduct( @RequestParam("prodNo") int prodNo, HttpServletRequest request, HttpServletResponse response , Model model ) throws Exception {
+		
+		System.out.println("/getProduct.do");
+		//Business Logic
+		Product product = productService.getProduct(prodNo);
+		
+		String cookieValue = "";
+		Cookie[] cookies = request.getCookies();
+		if(cookies != null) {
+			for(Cookie c : cookies) {
+				if(c.getName().equals("history")) {
+					cookieValue = c.getValue();
+				}
+			}
+		}
+		cookieValue += "/" + product.getProdNo();
+		
+		Cookie cookie = new Cookie("history", cookieValue);
+		cookie.setMaxAge(3600);
+		response.addCookie(cookie);
+		
+		// Model 과 View 연결
+		model.addAttribute("product", product);
+		
+		return "forward:/product/getProduct.jsp";
+	}
+	
+	@RequestMapping("/updateProductView.do")
+	public String updateProductView( @RequestParam("prodNo") int prodNo , Model model ) throws Exception{
+		System.out.println("/updateProductView.do");
+		//Business Logic
+		Product product = productService.getProduct(prodNo);
+		// Model 과 View 연결
+		model.addAttribute("product", product);
+		return "forward:/product/updateProduct.jsp";
+	}
+	
+	@RequestMapping("/updateUser.do")
+	public String updateProduct( @ModelAttribute("product") Product product , Model model , HttpSession session) throws Exception{
+
+		System.out.println("/updateProduct.do");
+		//Business Logic
+		productService.updateProduct(product);
+		
+		return "redirect:/getProduct.do?prodNo=" + product.getProdNo();
+	}
+	
+	@RequestMapping("/loginView.do")
+	public String loginView() throws Exception{
+		
+		System.out.println("/loginView.do");
+
+		return "redirect:/user/loginView.jsp";
+	}
+	
+	/*
+	@RequestMapping("/login.do")
+	public String login(@ModelAttribute("user") User user , HttpSession session ) throws Exception{
+		System.out.println("/login.do");
+		//Business Logic
+		User dbUser=userService.getUser(user.getUserId());
+		
+		if( user.getPassword().equals(dbUser.getPassword())){
+			session.setAttribute("user", dbUser);
+		}
+		
+		return "redirect:/index.jsp";
+	}
+	*/
+	
+	@RequestMapping("/logout.do")
+	public String logout(HttpSession session ) throws Exception{
+		
+		System.out.println("/logout.do");
+		
+		session.invalidate();
+		
+		return "redirect:/index.jsp";
+	}
+	/*
+	@RequestMapping("/checkDuplication.do")
+	public String checkDuplication( @RequestParam("userId") String userId , Model model ) throws Exception{
+		
+		System.out.println("/checkDuplication.do");
+		//Business Logic
+		boolean result=userService.checkDuplication(userId);
+		// Model 과 View 연결
+		model.addAttribute("result", new Boolean(result));
+		model.addAttribute("userId", userId);
+
+		return "forward:/user/checkDuplication.jsp";
+	}
+	*/
+
+}
